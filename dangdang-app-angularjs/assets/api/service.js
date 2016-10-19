561e29106bd50720bc2325d819f915064fc85acd
13027619968 <max@Q.local>
2016-10-19 17:53:02 +0800

    12
    12

--- dangdang-app-angularjs/assets/api/service.js
+++ dangdang-app-angularjs/assets/api/service.js
@@ -5,7 +5,7 @@ app.factory('commonService',['$http',function ($http) {
 
   service.getData = function(type,callBack){
     $http({
-      url:'../data/book_'+type+'.json',
+      url:'data/book_'+type+'.json',
       method:'get',
     })
     .then(function(res){
